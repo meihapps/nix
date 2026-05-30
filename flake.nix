e38ad6dffe2d430b5c1f5ed4efc6e2c533cca96f
mei happs <mail@meihapps.gay>
2026-05-30 23:18:48 +0000

    switch to cachyos fork of linux kernel

--- flake.nix
+++ flake.nix
@@ -1,5 +1,7 @@
 {
   inputs = {
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+    chaotic.inputs.nixpkgs.follows = "nixpkgs";
     home-manager = {
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -10,11 +12,13 @@
     };
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = inputs@{ self, home-manager, hyprland, nixpkgs, ... }: {
+  outputs = inputs@{ self, chaotic, home-manager, hyprland, nixpkgs, ... }: {
     nixosConfigurations.happuter = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
+        { nixpkgs.overlays = [ chaotic.overlays.cache-friendly ]; }
+        chaotic.nixosModules.nyx-cache
         ./boot.nix
         ./desktop.nix
         ./hardware.nix
