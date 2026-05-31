c767211c17a4d0c2bc196acedc37f958e4fc0adb
mei happs <mail@meihapps.gay>
2026-05-31 03:14:39 +0100

    fixed hyprland "use start-hyprland" nag

--- home.nix
+++ home.nix
@@ -390,7 +390,7 @@
     '';
     loginShellInit = ''
       if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-        exec Hyprland
+        exec start-hyprland
       end
     '';
     interactiveShellInit = ''
