09a7e64798aba36143e6f6005570261dd671d735
mei happs <mail@meihapps.gay>
2026-07-01 17:46:02 +0100

    add obsidian

--- devices/happuter/packages.nix
+++ devices/happuter/packages.nix
@@ -5,6 +5,7 @@
   environment.systemPackages = with pkgs; [
     inputs.agenix.packages.${pkgs.stdenv.hostPlatform.system}.default
     jdk21
+    obsidian
     prismlauncher
     rocmPackages.rocm-smi
   ];
