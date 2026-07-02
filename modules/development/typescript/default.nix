e9e7a8c50464699561a7d83c9944a86736b64e21
mei happs <mail@meihapps.gay>
2026-07-02 18:16:46 +0100

    add playwright

--- modules/development/typescript/default.nix
+++ modules/development/typescript/default.nix
@@ -2,7 +2,14 @@
 {
   home-manager.users.mei.home.packages = with pkgs; [
     nodejs
+    chromium
+    playwright-driver.browsers
   ];
+
+  environment.sessionVariables = {
+    PLAYWRIGHT_BROWSERS_PATH = "${pkgs.playwright-driver.browsers}";
+    PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = "true";
+  };
 
   home-manager.users.mei.programs.helix.languages.language = [
     {
