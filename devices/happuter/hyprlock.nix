9dd6796481eefac567ec6e62e5807989ead80da7
mei happs <mail@meihapps.gay>
2026-07-16 20:38:27 +0100

    new wallpaper

--- devices/happuter/hyprlock.nix
+++ devices/happuter/hyprlock.nix
@@ -1,6 +1,94 @@
 { lib, ... }:
 {
+  xdg.configFile."hypr/lockscreen.png".source = lib.mkForce ./lockscreen_5120x2160.png;
+
   xdg.configFile."hypr/hyprlock-device.conf".text = lib.mkForce ''
     $inputY = -47
+  '';
+
+  xdg.configFile."hypr/hyprlock.conf".text = lib.mkForce ''
+    source = $HOME/.config/hypr/mocha.conf
+
+    $accent = $mauve
+    $accentAlpha = $mauveAlpha
+    $font = FiraMono Nerd Font
+
+    general {
+      hide_cursor = true
+    }
+
+    background {
+      monitor =
+      path = $HOME/.config/hypr/lockscreen.png
+      blur_passes = 0
+      color = $base
+    }
+
+    label {
+      monitor =
+      text = Layout: $LAYOUT
+      color = $text
+      font_size = 25
+      font_family = $font
+      position = 30, -30
+      halign = left
+      valign = top
+    }
+
+    label {
+      monitor =
+      text = $TIME
+      color = $text
+      font_size = 90
+      font_family = $font
+      position = -30, 0
+      halign = right
+      valign = top
+    }
+
+    label {
+      monitor =
+      text = cmd[update:43200000] date +"%A, %d %B %Y"
+      color = $text
+      font_size = 25
+      font_family = $font
+      position = -30, -150
+      halign = right
+      valign = top
+    }
+
+    image {
+      monitor =
+      path = $HOME/.face
+      size = 100
+      border_color = $accent
+      position = 0, 75
+      halign = center
+      valign = center
+    }
+
+    source = $HOME/.config/hypr/hyprlock-device.conf
+
+    input-field {
+      monitor =
+      size = 300, 60
+      outline_thickness = 4
+      dots_size = 0.2
+      dots_spacing = 0.2
+      dots_center = true
+      outer_color = $accent
+      inner_color = $surface0
+      font_color = $text
+      fade_on_empty = false
+      placeholder_text = <span foreground="##$textAlpha"><i>󰌾 Logged in as </i><span foreground="##$accentAlpha">$USER</span></span>
+      hide_input = false
+      check_color = $accent
+      fail_color = $red
+      fail_text = <i>$FAIL <b>($ATTEMPTS)</b></i>
+      capslock_color = $yellow
+      position = 0, $inputY
+      halign = center
+      valign = center
+    }
   '';
 }
