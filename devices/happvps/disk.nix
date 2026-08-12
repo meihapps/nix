337adb72fdbe4516ae4da0a51da0a036e541746c
mei happs <mail@meihapps.gay>
2026-08-12 19:34:11 +0100

    added block drive to happvps

--- devices/happvps/disk.nix
+++ devices/happvps/disk.nix
@@ -1,27 +1,46 @@
 { ... }:
 {
-  disko.devices.disk.main = {
-    type = "disk";
-    device = "/dev/sda";
-    content = {
-      type = "gpt";
-      partitions = {
-        ESP = {
-          size = "512M";
-          type = "EF00";
-          content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = "/boot";
-            mountOptions = [ "umask=0077" ];
+  disko.devices.disk = {
+    main = {
+      type = "disk";
+      device = "/dev/sda";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size = "512M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "umask=0077" ];
+            };
+          };
+          root = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+            };
           };
         };
-        root = {
-          size = "100%";
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/";
+      };
+    };
+    data = {
+      type = "disk";
+      device = "/dev/disk/by-id/scsi-360fd569a53724c2583b095c32b6f94fc";
+      content = {
+        type = "gpt";
+        partitions = {
+          data = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/mnt/data";
+            };
           };
         };
       };
--- devices/happvps/hardware-configuration.nix
+++ devices/happvps/hardware-configuration.nix
@@ -9,9 +9,18 @@
       device = "nodev";
     };
   };
-  fileSystems."/boot/efi" = { device = "/dev/disk/by-uuid/BBF7-A736"; fsType = "vfat"; };
+  fileSystems = {
+    "/boot/efi" = {
+      device = "/dev/disk/by-uuid/BBF7-A736";
+      fsType = "vfat";
+    };
+    "/mnt/data" = {
+      device = "/dev/disk/by-uuid/d09bb755-12d0-46a2-a1fd-6b9ba08ada13";
+      fsType = "ext4";
+    };
+  };
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" ];
   boot.initrd.kernelModules = [ "nvme" ];
   fileSystems."/" = { device = "/dev/mapper/ocivolume-root"; fsType = "xfs"; };
-  
+
 }
