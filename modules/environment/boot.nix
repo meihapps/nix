fe9a6e0373bc19c4f4781e25ee868d9e0fca43f9
mei happs <mail@meihapps.gay>
2026-09-06 12:42:50 +0100

    cap generations to 5 and auto prune old generations

--- modules/environment/boot.nix
+++ modules/environment/boot.nix
@@ -2,7 +2,7 @@
 {
   boot.loader = {
     systemd-boot.enable = true;
-    systemd-boot.configurationLimit = 15;
+    systemd-boot.configurationLimit = 5;
     efi.canTouchEfiVariables = true;
   };
 }
--- modules/environment/nix.nix
+++ modules/environment/nix.nix
@@ -1,6 +1,15 @@
 { ... }:
 {
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nix.settings.auto-optimise-store = true;
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      auto-optimise-store = true;
+    };
+  };
   nixpkgs.config.allowUnfree = true;
 }
